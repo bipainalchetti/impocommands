41eafebe46dd48c58efc2e6d39d750c06c5c4611
Bipin Alchetti <30284935+bipainalchetti@users.noreply.github.com>
2023-09-27 00:10:44 +0200

    Update bashcmds.sh

--- bashcmds.sh
+++ bashcmds.sh
@@ -3,3 +3,5 @@ PS1='\[\e]0;\u@\h: \w\a\]\[\033[32m\]\u@\h \[\033[35m\]\w\[\033[00m\]$(git branc
 
 #Command to create and label k8s namespace
 kubectl create namespace my-namespace --dry-run=client -o yaml | kubectl apply -f - --namespace=my-namespace -l app=myapp
+kubectl create namespace my-namespace --dry-run=client -o yaml | kubectl apply -f -
+
