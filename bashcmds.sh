a70b9df63f92db0ef7bc94f9db994efb54cc8f64
Bipin Alchetti <30284935+bipainalchetti@users.noreply.github.com>
2023-09-26 23:36:51 +0200

    Update bashcmds.sh

--- bashcmds.sh
+++ bashcmds.sh
@@ -1,2 +1,5 @@
 #Command to add branch name in linux shell
 PS1='\[\e]0;\u@\h: \w\a\]\[\033[32m\]\u@\h \[\033[35m\]\w\[\033[00m\]$(git branch 2>/dev/null | grep "^*" | sed "s/* //") \$ '
+
+#Command to create and label k8s namespace
+kubectl create namespace my-namespace --dry-run=client -o yaml | kubectl apply -f - --namespace=my-namespace -l app=myapp
